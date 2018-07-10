f00b3687c5cb50f34ab25b9c6b7ee9d906a81467
Zeydi <oussema.zeidi@ieee.org>
2018-07-10 16:23:38 +0200

    updated store creation

--- public/store.js
+++ public/store.js
@@ -1,18 +1,14 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
 // Creating Store
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(thunk)
 );
 
 export default store;
